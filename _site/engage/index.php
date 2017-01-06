f60503a236c2b9adf65a3c48e02ffca4bf5c6c02
[thecubix] <[timon.forrer@gmail.com]>
2017-01-06 21:41:45 +0100

    Jekyll build

--- _site/engage/index.php
+++ _site/engage/index.php
@@ -94,6 +94,15 @@ if ((!empty($firstName)) && (!empty($lastName)) && (!empty($email)) && (!empty($
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-86605317-2', 'auto');
+            ga('send', 'pageview');
+        </script>
     </head>
     <body>
         <div id="fb-root"></div>
@@ -104,15 +113,6 @@ if ((!empty($firstName)) && (!empty($lastName)) && (!empty($email)) && (!empty($
                 js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.8";
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));</script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-86605317-2', 'auto');
-            ga('send', 'pageview');
-        </script>
         
         <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
     <div class="container">
@@ -206,7 +206,7 @@ if ((!empty($firstName)) && (!empty($lastName)) && (!empty($email)) && (!empty($
                 <h1>Voltage Arc</h1>
                 <p><a href="https://maps.google.com/?q=Luzernerstrasse+14,+5712+Beinwil+am+See" target="_blank">Luzernerstrasse 14 / Postfach 50</a></p>
                 <p><a href="https://maps.google.com/?q=Luzernerstrasse+14,+5712+Beinwil+am+See" target="_blank">5712 Beinwil am See</a></p>
-                <p><a href="tel:"></a></p>
+                <p><a href="tel:0796898660">079 689 86 60</a></p>
             </div>
             <div class="social col-lg-4 col-md-4 col-sm-4 col-xs-12">
                 <h1>Folge uns!</h1>
