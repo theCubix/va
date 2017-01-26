16d57dadb1924b6c2f3365bf4dee4b139c34a5fc
[thecubix] <[timon.forrer@gmail.com]>
2017-01-26 20:27:23 +0100

    Jekyll build

--- _site/engage/index.php
+++ _site/engage/index.php
@@ -75,8 +75,10 @@ if ((!empty($firstName)) && (!empty($lastName)) && (!empty($email)) && (!empty($
 <html lang="de">
     <head>
         
+        
         <base href="http://www.voltagearc.com">
         
+		
         
         <meta property="og:image" content="http://www.voltagearc.com/uploads/drums.jpg"/>
         
